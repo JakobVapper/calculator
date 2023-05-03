aa714ac104e771f9a68303f747643f541219e894
JakobVapper <jakob.vapper@ametikool.ee>
2023-05-03 14:03:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+function calculate() {
+	const sleepHours = parseInt(document.getElementById("sleep").value);
+	const socialHours = parseInt(document.getElementById("social").value);
+	const exerciseHours = parseInt(document.getElementById("exercise").value);
+	const studyHours = parseInt(document.getElementById("study").value);
+	const otherHours = parseInt(document.getElementById("other").value);
+
+	const totalHours = sleepHours + socialHours + exerciseHours + studyHours + otherHours;
+
+	if (totalHours > 24) {
+		alert("The total number of hours cannot be more than 24.");
+		return;
+	}
+
+	const remainingHours = 24 - totalHours;
+
+	let resultText = `You have ${remainingHours} hours left in the day.`;
+
+	if (sleepHours >= 8) {
+		resultText += " You have a healthy amount of sleep!";
+	}
+
+	if (socialHours >= 8) {
+		resultText += " You are socializing a full day!";
+	}
+
+	if (exerciseHours >= 1) {
+		resultText += " You are staying active!";
+	}
+
+	if (studyHours >= 2) {
+		resultText += " You are dedicating time to learning!";
+	}
+
+	if (otherHours > 0) {
+		resultText += ` You have ${otherHours} hours for other activities.`;
+	}
+
+	document.getElementById("result").innerHTML = resultText;
+}
